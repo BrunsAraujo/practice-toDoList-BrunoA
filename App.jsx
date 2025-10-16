e7cc6ee0c859bf0319746558a170053837a4973d
Bruno Araujogit <bruno.correadearaujo@gmail.com>
2025-10-16 14:56:40 -0500

    ToDoList React

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header.jsx"
+import ToDoList from "./Components/ToDoList.jsx"
+import './app.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header/>
+        <ToDoList/>
+      </div>
+      </>
+  )
+}
+export default App
